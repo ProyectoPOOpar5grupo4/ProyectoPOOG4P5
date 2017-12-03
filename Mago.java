7453f40323a8373bf12fb75e0641dc2affd3452d
Carla Sánchez <33770887+carmsanc@users.noreply.github.com>
2017-12-03 18:15:28 -0500

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -2,6 +2,10 @@ package proyecto;
 
 
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 
 /**
  * 
@@ -27,14 +31,83 @@ public class Mago {
         this.Tipo = Tipo;
     }
      
-    public Void Login() {
-        
-        return null;
+    public static String[] Login() {
+        Scanner sc = new Scanner(System.in);
+        String usernom = "", userapell = "", userrol = "";
+        String[] userdata = new String[3];
+        int checknum = -1;
+
+        while (checknum == -1) {
+            System.out.println("Usuario: ");
+            String usuario = sc.nextLine();
+            System.out.println("Contraseña: ");
+            String password = sc.nextLine();
+            File archivo = null;
+            FileReader fr = null;
+            BufferedReader br = null;
+            String linea;
+            try {
+                fr = new FileReader("Usuarios.txt");
+                br = new BufferedReader(fr);
+                while ((linea = br.readLine()) != null) {
+                    String texto[] = linea.split(",");
+                    if (texto[0].equals(usuario) && texto[1].equals(password)) {
+                        checknum = 1;
+                        usernom = texto[2];
+                        userapell = texto[3];
+                        userrol = texto[4];
+                    }
+                }
+                br.close();
+            } catch (Exception ex) {
+                System.out.println("Error");
+            }
+            if (checknum == -1) {
+                System.out.println("El usuario o la contraseña no son válidos. Por favor intente otra vez.");
+            } else {
+                System.out.println("\nIngreso exitoso\n");
+
+            }
+        }
+
+        userdata[0] = usernom;
+        userdata[1] = userapell;
+        userdata[2] = userrol;
+
+        return userdata;
+    }
+    
+    public static void logOut() {
+        System.out.print("\nHa salido del sistema.\n");
+    }
+      public String getNombres() {
+        return Nombres;
     }
 
-    @Override
-    public String toString(){
-        return Nombres+" "+Apellidos+" -- "+Edad;
-        
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getVarita() {
+        return Varita;
+    }
+
+    public void setVarita(String Varita) {
+        this.Varita = Varita;
     }
 }
