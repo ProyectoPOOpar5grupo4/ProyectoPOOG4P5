dd51a3d666a1d7a41207f3bdba42e1e619b8ffa0
BriggitteL <33633208+BriggitteL@users.noreply.github.com>
2017-12-03 17:38:33 -0500

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -1,6 +1,6 @@
 package proyecto;
 
-
+//importando el paquete java.util
 import java.util.*;
 
 /**
@@ -8,7 +8,7 @@ import java.util.*;
  */
 public class Mago {
 
-    
+    //Declaracion de los atributos de la clase Mago
     public String Usuario;
     public String Contraseña;
     public String Nombres;
@@ -18,6 +18,7 @@ public class Mago {
     public String Varita;
     public Tipo_Mago Tipo;
     
+    //Declaración de métodos 
     public Mago(String Nombres, String Apellidos, int Edad, String Casa, String Varita, Tipo_Mago Tipo) {
         this.Nombres = Nombres;
         this.Apellidos = Apellidos;
